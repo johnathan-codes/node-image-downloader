eaaeb6cafc7cbd138f4db482aad658461aa991a3
OneManDevz <42937562+OneManDevz@users.noreply.github.com>
2020-07-09 22:07:12 +0200

    ReadMe

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+create folder 'data'
+
+create file 'constants.js'
+with contents like these:
+```javascript
+const url = '<Your URL';
+const classToFind = '<your class>';
+
+
+module.exports = {
+  url,
+  classToFind
+}
+```
--- main.js
+++ main.js
@@ -3,7 +3,12 @@ const rp = require('request-promise');
 const helpers = require('./data/constants')
 
 rp(helpers.url).then(response => {
-  fs.writeFile('data/response-body.html', response, (err) => {if(err) return console.log(err); console.log('Done!')})
+  fs.writeFile('data/response-body.html', response, (err) => {
+    if(err) {
+      return console.log(err)
+    }
+    console.log('Done!')
+  })
 }).catch(err => {
   console.log(err)
 })
