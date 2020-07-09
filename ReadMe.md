daedf0a3b33493a81458098a7b70133a1cdd32e4
OneManDevz <42937562+OneManDevz@users.noreply.github.com>
2020-07-09 23:09:34 +0200

    download imgs by class from source

--- ReadMe.md
+++ ReadMe.md
@@ -4,7 +4,7 @@ create file 'constants.js'
 with contents like these:
 ```javascript
 const url = '<Your URL';
-const classToFind = '<your class>';
+const classToFind = '.<your class>'; // The dot is necessary as we are looking for class
 
 
 module.exports = {
--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 const fs = require('fs')
-const rp = require('request-promise');
+const rp = require('request-promise')
 const helpers = require('./data/constants')
+const cheerio = require('cheerio')
+const axios = require('axios')
 
 rp(helpers.url).then(response => {
-  fs.writeFile('data/response-body.html', response, (err) => {
-    if(err) {
-      return console.log(err)
-    }
-    console.log('Done!')
+  //getHtmlForTesting(response) //This is optional - just for getting whole html page
+
+  let $ = cheerio.load(response)
+  $(helpers.classToFind).each((index, element) => {
+    download(element.attribs['data-src'], index)
   })
+  console.log('Done!')
 }).catch(err => {
   console.log(err)
 })
+
+const getHtmlForTesting = (data) => {
+  fs.writeFile('data/response-html.html', data, (err) => {
+    if(err) {
+      return console.log(err)
+    }
+  })
+}
+
+const download = (url, index) => {
+  axios({
+    url,
+    responseType: 'stream'
+  }).then(response => {
+    new Promise((resolve, reject) => {
+      response.data
+        .pipe(fs.createWriteStream(` ${helpers.url.split('/').pop()} - ${index}.jpg`)) //get last part of url for dynamic file name)
+        .on('finish', () => resolve())
+        .on('error', err => reject(err))
+    })
+  })
+}
