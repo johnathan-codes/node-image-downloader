b4c481968f84f4c549a5e7ba5f79d6e304c12954
OneManDevz <42937562+OneManDevz@users.noreply.github.com>
2020-07-09 23:26:01 +0200

    create download folders

--- main.js
+++ main.js
@@ -6,24 +6,36 @@ const axios = require('axios')
 
 rp(helpers.url).then(response => {
   let $ = cheerio.load(response)
+  makeFolders(helpers.url.split('/').pop())
   $(helpers.classToFind).each((index, element) => {
     download(element.attribs['data-src'], index)
   })
-  console.log('Done!')
+  console.log('Done downloading!')
 }).catch(err => {
   console.log(err)
 })
 
 const download = (url, index) => {
-  axios({
-    url,
-    responseType: 'stream'
-  }).then(response => {
+  axios({url, responseType: 'stream'}).then(response => {
     new Promise((resolve, reject) => {
       response.data
-        .pipe(fs.createWriteStream(` ${helpers.url.split('/').pop()} - ${index}.jpg`)) //get last part of url for dynamic file name)
+        .pipe(fs.createWriteStream(`downloads/${helpers.url.split('/').pop()}/${helpers.url.split('/').pop()} - ${index}.jpg`))
         .on('finish', () => resolve())
         .on('error', err => reject(err))
     })
   })
+}
+
+const makeFolders = (folderName) => {
+  if(!fs.existsSync('downloads')){
+    fs.mkdir('downloads', (err) => console.log('mkdir downloads - Error:', err))
+  } else {
+    console.log('Folder "downloads" already exists. Skipped mkdir')
+  }
+
+  if(!fs.existsSync(`downloads/${folderName}`)){
+    fs.mkdir(`downloads/${folderName}`, (err) => console.log('mkdir downloads/dest - Error:', err))
+  } else {
+    console.log(`Folder "downloads/${folderName}" already exists. Skipped mkdir`)
+  }
 }
